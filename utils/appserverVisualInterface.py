6b339d37778e408cd3543bc8a7805c1a09b2d412
gabrieldss808 <gabrieldss808@gmail.com>
2020-06-27 17:20:30 -0300

    Continuando o card do Appserver
    Estou trabalhando em um laoyt legal que comporte todas as informações

--- utils/appserverVisualInterface.py
+++ utils/appserverVisualInterface.py
@@ -8,11 +8,14 @@ class AppserverVisual(BoxLayout):
     
     appserverInput = appserver
     NumberPositionView = 0
+    ColorServiceStatus = ''
 
     def __init__(self, app=appserver, NumberPosition=0 ,**kwargs):
         super().__init__(**kwargs)
 
         self.appserverInput = app
+
+        self.ColorServiceStatus = kivy.utils.get_color_from_hex("#34A853")
         
         self.NumberPositionView = NumberPosition
 
@@ -22,8 +25,19 @@ class AppserverVisual(BoxLayout):
         
         self.ids.lbPositionView.text = '# ' + str(self.NumberPositionView)
         self.ids.DirAppServer.text = self.appserverInput.cdir
+        
+        if(self.appserverInput.lServiceExists):
+            self.ids.lbServiceStatus.text = 'Serviço encontrado e rodando ;)\nNome: ' + self.appserverInput.cDisplayNameService
+            self.ids.lbServicePort.text = 'Porta: ' + self.appserverInput.cport
+            self.ColorServiceStatus = kivy.utils.get_color_from_hex("#34A853")
+        else:
+            self.ids.lbServiceStatus.text = 'Tag de serviço do Appserver não está instalado :(\nNome: ' + self.appserverInput.cDisplayNameService 
+            self.ColorServiceStatus = kivy.utils.get_color_from_hex("#DB4437")
 
-
+    def ColorServiceStatus(self):
+        
+        return self.ColorServiceStatus
+        
     def BtOpenDir(self):
 
         subprocess.Popen('explorer ' + self.appserverInput.cdir.rstrip('appserver.ini'))
