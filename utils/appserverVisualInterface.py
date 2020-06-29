042790b47104f5464fb4ae28d99630152d9423e2
gabrieldss808 <gabrieldss808@gmail.com>
2020-06-29 16:56:51 -0300

    Fiz varias alteraões
    Fiz varias alterações, porém vou ter que parar o projeto por enquando, isso porque o kivy usa Open GL e no caso, como é voltado para rodar em servidores, provavelmente não vai ter isso neles, então vou fazer um projeto menor usando o Tkinter e talvez volte nesse usando esse FrameWork

--- utils/appserverVisualInterface.py
+++ utils/appserverVisualInterface.py
@@ -1,26 +1,44 @@
 import kivy
 import subprocess
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ColorProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from utils.classChildrens import appserver, smartclient, RPO
+
 
 class AppserverVisual(BoxLayout):
     
     appserverInput = appserver
     NumberPositionView = 0
-    ColorServiceStatus = ''
+    ColorServiceStatus = ColorProperty
 
     def __init__(self, app=appserver, NumberPosition=0 ,**kwargs):
-        super().__init__(**kwargs)
 
         self.appserverInput = app
-
-        self.ColorServiceStatus = kivy.utils.get_color_from_hex("#34A853")
         
         self.NumberPositionView = NumberPosition
 
+        if(self.appserverInput.lServiceExists):
+
+            self.ColorServiceStatus = kivy.utils.get_color_from_hex("#34A853")
+        else:
+
+            self.ColorServiceStatus = kivy.utils.get_color_from_hex("#DB4437")
+        
+        super().__init__(**kwargs)
+        
         self.injectData()
     
+
+    def GetColorServiceStatus(self):
+
+        if(self.ColorServiceStatus == []):
+
+            return kivy.utils.get_color_from_hex("#34A853")
+        else:
+
+            return self.ColorServiceStatus
+
     def injectData(self):
         
         self.ids.lbPositionView.text = '# ' + str(self.NumberPositionView)
@@ -29,14 +47,10 @@ class AppserverVisual(BoxLayout):
         if(self.appserverInput.lServiceExists):
             self.ids.lbServiceStatus.text = 'Serviço encontrado e rodando ;)\nNome: ' + self.appserverInput.cDisplayNameService
             self.ids.lbServicePort.text = 'Porta: ' + self.appserverInput.cport
-            self.ColorServiceStatus = kivy.utils.get_color_from_hex("#34A853")
         else:
-            self.ids.lbServiceStatus.text = 'Tag de serviço do Appserver não está instalado :(\nNome: ' + self.appserverInput.cDisplayNameService 
-            self.ColorServiceStatus = kivy.utils.get_color_from_hex("#DB4437")
+            self.ids.lbServiceStatus.text = 'Tag de serviço do Appserver não está instalado :(\nNome: ' + self.appserverInput.cDisplayNameService
 
-    def ColorServiceStatus(self):
-        
-        return self.ColorServiceStatus
+        self.ids.lbContentAppserverIni.text = self.appserverInput.cContent
         
     def BtOpenDir(self):
 
